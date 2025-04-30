b2dd767a01a962113c07d3d8a76b50df912c798a
nnarct <nannapat1551@gmail.com>
2025-04-30 14:01:48 +0700

    chore: run format

--- frontend/src/api/dashboard.api.ts
+++ frontend/src/api/dashboard.api.ts
@@ -1,6 +1,6 @@
 import { axios } from '@/config/axiosConfig';
 
-export const fetchAdminDashboardData =  async () => {
+export const fetchAdminDashboardData = async () => {
   try {
     const response = await axios.get('/dashboard/admin');
     return response.data.data;
