97ebb5c153812ddf2cc0443d99a0c43fe67598f2
nnarct <nannapat1551@gmail.com>
2025-04-30 18:24:26 +0700

    feat(ui): add modal to inform users that save functionality is currently unavailable

--- frontend/src/components/feature/StlDisplay/MenuBar/SaveButton.tsx
+++ frontend/src/components/feature/StlDisplay/MenuBar/SaveButton.tsx
@@ -1,8 +1,29 @@
+import { Modal } from 'antd';
+import { useState } from 'react';
 import MenuButton from './MenuButton';
-import { PiFloppyDiskBackDuotone } from "react-icons/pi";
+import { PiFloppyDiskBackDuotone } from 'react-icons/pi';
 
 const SaveButton = () => {
-  return <MenuButton icon={<PiFloppyDiskBackDuotone />} tooltip='Save Model' text='Save'/>
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <MenuButton icon={<PiFloppyDiskBackDuotone />} onClick={handleClick} tooltip='Save Model' text='Save' />
+      <Modal
+        open={isOpen}
+        onOk={() => setIsOpen(false)}
+        onCancel={() => setIsOpen(false)}
+        title='Save Unavailable'
+        okText='OK'
+        cancelButtonProps={{ style: { display: 'none' } }}
+      >
+        <p>We apologize, but saving the model is currently unavailable.</p>
+        <p>Please try again later or contact support if the issue persists.</p>
+      </Modal>
+    </>
+  );
 };
 
 export default SaveButton;
