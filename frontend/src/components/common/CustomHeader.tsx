e71f1a04f078fccdd2717c21eabd5a8fa18d81a1
nnarct <nannapat1551@gmail.com>
2025-05-02 11:41:02 +0700

    feat: sticky header with back option

--- frontend/src/components/common/CustomHeader.tsx
+++ frontend/src/components/common/CustomHeader.tsx
@@ -1,14 +1,24 @@
-import { Layout } from "antd";
+import { Button, Layout } from 'antd';
+import { IoIosArrowBack } from 'react-icons/io';
+import { type To, useNavigate } from 'react-router-dom';
 
 type Props = React.ComponentProps<typeof Layout.Header>;
+type HeaderWithBackProps = Props & { backTo?: To | number };
 
-const CustomHeader = ({ children, ...rest }: Props) => {
+const CustomHeader = ({ children, backTo, ...rest }: HeaderWithBackProps) => {
+  if (backTo) {
+    return <HeaderWithBack backTo={backTo as To} />;
+  }
+
   return (
     <Layout.Header
       className='flex items-center'
       style={{
         background: 'white',
+        position: 'sticky',
         height: '73px',
+        top: 0,
+        zIndex: 1000,
         padding: '0 24px',
         borderBottom: '1px solid rgba(5, 5, 5, 0.05)',
         borderLeft: '1px solid rgba(5, 5, 5, 0.05)',
@@ -20,4 +30,36 @@ const CustomHeader = ({ children, ...rest }: Props) => {
     </Layout.Header>
   );
 };
+
 export default CustomHeader;
+
+const HeaderWithBack = ({ children, backTo, ...rest }: HeaderWithBackProps) => {
+  const navigate = useNavigate();
+  return (
+    <Layout.Header
+      className='flex items-center'
+      style={{
+        background: 'white',
+        position: 'sticky',
+        top: 0,
+        zIndex: 1000,
+        height: '73px',
+        padding: '0 24px',
+        borderBottom: '1px solid rgba(5, 5, 5, 0.05)',
+        borderLeft: '1px solid rgba(5, 5, 5, 0.05)',
+        ...(rest.style || {}),
+      }}
+      {...rest}
+    >
+      <Button
+        onClick={() => navigate(backTo as To)}
+        type='text'
+        icon={<IoIosArrowBack />}
+        style={{ padding: '0 16px 0 0' }}
+      >
+        Back
+      </Button>
+      {children}
+    </Layout.Header>
+  );
+};
