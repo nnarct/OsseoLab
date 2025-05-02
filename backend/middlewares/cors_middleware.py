5cfda56c2d53f5e7dfca760a83a6d8633888205d
nnarct <nannapat1551@gmail.com>
2025-05-02 11:37:15 +0700

    chore: patch cors config

--- backend/middlewares/cors_middleware.py
+++ backend/middlewares/cors_middleware.py
@@ -3,6 +3,6 @@ from flask import request
 def add_cors_headers(response):
     response.headers["Access-Control-Allow-Origin"] = request.headers.get("Origin", "*")
     response.headers["Access-Control-Allow-Credentials"] = "true"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS, PATCH"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     return response
