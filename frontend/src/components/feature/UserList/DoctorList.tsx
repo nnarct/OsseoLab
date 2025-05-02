2dd4668ca2de2e7a6ddec0f2d67f7fba3c4a547c
nnarct <nannapat1551@gmail.com>
2025-05-02 11:50:14 +0700

    refactor[doctor]: improve surgeon list layout with custom header and cleaner structure

--- frontend/src/components/feature/UserList/DoctorList.tsx
+++ frontend/src/components/feature/UserList/DoctorList.tsx
@@ -1,8 +1,9 @@
 import { useGetDoctors } from '@/services/admin/user.service';
-import { Card, Table, Input } from 'antd';
+import { Card, Table, Input, Layout } from 'antd';
 import { useState, useMemo } from 'react';
 import { userColumns } from '@/components/feature/UserList/userColumns';
 import CreateDoctorModal from './CreateDoctorModal';
+import CustomHeader from '@/components/common/CustomHeader';
 
 const DoctorList = () => {
   const { data, isLoading } = useGetDoctors();
@@ -13,29 +14,42 @@ const DoctorList = () => {
     return data.filter((item) => {
       const query = searchTerm.toLowerCase();
       return (
-        (`${item.firstname} ${item.lastname}`).toLowerCase().includes(query) ||
+        `${item.firstname} ${item.lastname}`.toLowerCase().includes(query) ||
         item.firstname?.toLowerCase().includes(query) ||
         item.lastname?.toLowerCase().includes(query) ||
         item.email?.toLowerCase().includes(query) ||
-        item.phone?.toLowerCase().includes(query)||
+        item.phone?.toLowerCase().includes(query) ||
         item.username?.toLowerCase().includes(query)
       );
     });
   }, [data, searchTerm]);
 
   return (
-    <Card title='Surgeon List'>
-      <div className='flex w-full justify-between items-center pb-4'>
-        <Input.Search
-          placeholder='Search by name, username, email, or mobile'
-          allowClear
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className='max-w-sm'
-        />
-        <CreateDoctorModal />
-      </div>
-      <Table dataSource={filteredData} columns={userColumns} loading={isLoading} rowKey={'id'} scroll={{ x: 'auto' }} />
-    </Card>
+    <>
+      <CustomHeader>
+        <p className='text-2xl font-bold'>Surgeon List</p>
+      </CustomHeader>
+      <Layout.Content className='p-4'>
+        <Card title='Surgeon List'>
+          <div className='flex w-full items-center justify-between pb-4 gap-x-4'>
+            <Input.Search
+              placeholder='Search by name, username, email, or mobile'
+              allowClear
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className='max-w-sm'
+            />
+            <CreateDoctorModal />
+          </div>
+          <Table
+            dataSource={filteredData}
+            columns={userColumns}
+            loading={isLoading}
+            rowKey={'id'}
+            scroll={{ x: 'auto' }}
+          />
+        </Card>
+      </Layout.Content>
+    </>
   );
 };
 
