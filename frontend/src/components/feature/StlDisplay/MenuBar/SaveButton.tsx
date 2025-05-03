ea96ff8b95e2daad0df2206bbe58e8e021737db3
nnarct <nannapat1551@gmail.com>
2025-05-03 16:30:48 +0700

    chore: update save button modal text for ui demo

--- frontend/src/components/feature/StlDisplay/MenuBar/SaveButton.tsx
+++ frontend/src/components/feature/StlDisplay/MenuBar/SaveButton.tsx
@@ -1,17 +1,33 @@
-import { Modal } from 'antd';
+import { message, Modal } from 'antd';
 import { useState } from 'react';
 import MenuButton from './MenuButton';
 import { PiFloppyDiskBackDuotone } from 'react-icons/pi';
 
 const SaveButton = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [messageApi, context] = message.useMessage();
   const handleClick = () => {
-    setIsOpen(!isOpen);
+    setIsOpen(true);
   };
   return (
     <>
+      {context}
       <MenuButton icon={<PiFloppyDiskBackDuotone />} onClick={handleClick} tooltip='Save Model' text='Save' />
       <Modal
+        open={isOpen}
+        centered
+        onOk={() => {
+          messageApi.success('Model saved successfully!');
+          setIsOpen(false);
+        }}
+        onCancel={() => setIsOpen(false)}
+        title='Confirm Save'
+        okText='Save'
+        cancelText='Cancel'
+      >
+        <p>Are you sure you want to save the model?</p>
+      </Modal>
+      {/* <Modal
         open={isOpen}
         onOk={() => setIsOpen(false)}
         onCancel={() => setIsOpen(false)}
@@ -21,7 +37,7 @@ const SaveButton = () => {
       >
         <p>We apologize, but saving the model is currently unavailable.</p>
         <p>Please try again later or contact support if the issue persists.</p>
-      </Modal>
+      </Modal> */}
     </>
   );
 };
