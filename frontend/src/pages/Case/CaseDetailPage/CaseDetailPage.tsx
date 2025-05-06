0d835e44b0f478cda5d1b58a382bdcc8c18e54b9
nnarct <nannapat1551@gmail.com>
2025-05-06 18:20:48 +0700

    feat: support case detail page for technician role

--- frontend/src/pages/Case/CaseDetailPage/CaseDetailPage.tsx
+++ frontend/src/pages/Case/CaseDetailPage/CaseDetailPage.tsx
@@ -1,12 +1,14 @@
 import { useAuth } from '@/hooks/useAuth';
 import CaseDetailPageAdmin from './CaseDetailPageAdmin';
 import CaseDetailPageDoctor from './CaseDetailPageDoctor';
+import CaseDetailTechnicianPage from './CaseDetailPageTechnician';
 
 const CaseDetailPage = () => {
   const { role } = useAuth();
 
   if (role === 'admin') return <CaseDetailPageAdmin />;
   if (role === 'doctor') return <CaseDetailPageDoctor />;
+  if (role === 'technician') return <CaseDetailTechnicianPage />;
   return <div>No access</div>;
 };
 
