83cd7f5dc336bce66b801804de7fe5fb03731a06
nnarct <nannapat1551@gmail.com>
2025-05-17 02:51:52 +0700

    fix[item-panel]: prevent panel from closing when clicking inside color picker

--- frontend/src/components/feature/StlDisplay/ItemList/ItemListPanel.tsx
+++ frontend/src/components/feature/StlDisplay/ItemList/ItemListPanel.tsx
@@ -4,21 +4,25 @@ import ListPanel from './ListPanel';
 import { useItemListPanelStateStore } from '@/store/useItemListPanelStateStore';
 
 const ItemListPanel = () => {
-    const { setIsOpen } = useItemListPanelStateStore();
-  
+  const { setIsOpen } = useItemListPanelStateStore();
+
   const panelRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (panelRef.current && !panelRef.current.contains(event.target as Node)) {
-        setIsOpen(false);
-      }
+      const target = event.target as Node;
+
+      if (panelRef.current && panelRef.current.contains(target)) return;
+
+      const popup = document.querySelector('.ant-popover');
+      if (popup && popup.contains(target)) return;
+      setIsOpen(false);
     };
 
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, []);
+  }, [setIsOpen]);
 
   return (
     <div ref={panelRef} className='relative'>
