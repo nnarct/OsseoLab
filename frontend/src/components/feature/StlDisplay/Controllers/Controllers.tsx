ae02bbad66062c5faf5a768020e78513cd707347
nnarct <nannapat1551@gmail.com>
2025-05-28 16:12:32 +0700

    feat: rotatable model for visual preview

--- frontend/src/components/feature/StlDisplay/Controllers/Controllers.tsx
+++ frontend/src/components/feature/StlDisplay/Controllers/Controllers.tsx
@@ -4,34 +4,44 @@ import { useStlDisplay } from '@/hooks/useStlDisplay';
 import Lighting from './Lighting';
 import { MIN_ZOOM, MAX_ZOOM } from '@/constants/stlDisplay';
 
-const Controllers = () => {
+interface ControllersProps {
+  isPreviewing?: boolean;
+}
+
+const Controllers = ({ isPreviewing = false }: ControllersProps) => {
   const { sceneHandlerRef } = useStlDisplay();
 
   return (
     <>
-      <OrbitControls
-        makeDefault
-        minDistance={MIN_ZOOM}
-        maxDistance={MAX_ZOOM}
-        ref={(ref) => {
-          if (ref) {
-            const camera = ref.object;
-            if (camera instanceof THREE.PerspectiveCamera) {
-              sceneHandlerRef.current = {
-                ...sceneHandlerRef.current,
-                camera,
-                controls: ref,
-              };
+      <group>
+        <OrbitControls
+          makeDefault
+          minDistance={MIN_ZOOM}
+          maxDistance={MAX_ZOOM}
+          enableZoom={true}
+          enablePan={false}
+          autoRotate={isPreviewing}
+          autoRotateSpeed={5}
+          ref={(ref) => {
+            if (ref) {
+              const camera = ref.object;
+              if (camera instanceof THREE.PerspectiveCamera) {
+                sceneHandlerRef.current = {
+                  ...sceneHandlerRef.current,
+                  camera,
+                  controls: ref,
+                };
+              }
             }
-          }
-        }}
-      />
+          }}
+        />
+      </group>
 
       <GizmoHelper alignment='bottom-right' margin={[80, 80]}>
         <GizmoViewcube />
       </GizmoHelper>
 
-      <primitive
+      {/* <primitive
         object={(() => {
           const axesHelper = new THREE.AxesHelper(150);
           (axesHelper.material as THREE.LineBasicMaterial).linewidth = 9; // Note: may not work in most browsers
@@ -39,7 +49,7 @@ const Controllers = () => {
           // X red, Y green, Z blue
           return axesHelper;
         })()}
-      />
+      /> */}
       <Lighting />
     </>
   );
