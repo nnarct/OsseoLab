9c8b80e35fbfb175c74bac9be912c81e70afd10f
nnarct <nannapat1551@gmail.com>
2025-06-12 03:02:38 +0700

    refactor: remove unused ArrowHelper for plane visualization

--- frontend/src/components/feature/StlDisplay/ClippingPlane/ClippingPlane.tsx
+++ frontend/src/components/feature/StlDisplay/ClippingPlane/ClippingPlane.tsx
@@ -1,5 +1,4 @@
 import { TransformControls } from '@react-three/drei';
-import { useEffect } from 'react';
 import * as THREE from 'three';
 import type { TransformControlsMode } from '@/types/stlDisplay';
 
@@ -28,41 +27,41 @@ const ClippingPlane = ({
   show: boolean;
   meshRef: React.RefObject<THREE.Mesh>;
 }) => {
-  const planeRef = meshRef;
   const { planeHandler, tool } = useStlDisplay();
-  const updatePlane = usePlaneUpdater(plane, id);
   const { camera } = useThree();
+  const updatePlane = usePlaneUpdater(plane, id);
   const isActive = id === planeHandler.activePlaneId && tool.current === 'plane';
 
-  const { transformRef, domElement } = useTransformControls(isActive, planeRef, () => updatePlane(planeRef.current));
+  const { transformRef, domElement } = useTransformControls(isActive, meshRef, () => updatePlane(meshRef.current));
   const material = createClippingPlaneMaterial(frontColor, backColor, opacity);
-  useEffect(() => {
-    if (planeRef.current) {
-      const mesh = planeRef.current;
-      const origin = mesh.getWorldPosition(new THREE.Vector3());
-      const normal = new THREE.Vector3(0, 0, -1)
-        .applyQuaternion(mesh.getWorldQuaternion(new THREE.Quaternion()))
-        .normalize();
+  // useEffect(() => {
+  //   if (!planeRef.current) {
+  //     return;
+  //   }
+  //   // const mesh = planeRef.current;
+  //   // const origin = mesh.getWorldPosition(new THREE.Vector3());
+  //   // const normal = new THREE.Vector3(0, 0, -1)
+  //   // .applyQuaternion(mesh.getWorldQuaternion(new THREE.Quaternion()))
+  //   // .normalize();
 
-      const arrowHelper = new THREE.ArrowHelper(normal, origin, 30, 0xff0000);
-      mesh.add(arrowHelper);
-    }
-  }, [planeRef]);
+  //   // const arrowHelper = new THREE.ArrowHelper(normal, origin, 30, 0xff0000);
+  //   // mesh.add(arrowHelper);
+  // }, [planeRef]);
 
   return (
     <>
       {isActive && (
         <TransformControls
           ref={transformRef}
-          object={planeRef.current || undefined}
+          object={meshRef.current || undefined}
           mode={mode}
           camera={camera}
           domElement={domElement}
-          onMouseUp={() => updatePlane(planeRef.current)}
+          onMouseUp={() => updatePlane(meshRef.current)}
           onObjectChange={() => {}}
         />
       )}
-      <mesh ref={planeRef} userData={{ type: 'clippingPlane' }} name={id} visible={show}>
+      <mesh ref={meshRef} userData={{ type: 'clippingPlane', id }} name={id} visible={show}>
         <planeGeometry args={[150, 150]} />
         <primitive object={material ?? undefined} />
       </mesh>
