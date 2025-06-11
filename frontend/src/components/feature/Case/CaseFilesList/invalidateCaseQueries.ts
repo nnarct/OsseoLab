976ad168864818d68aa6141724ee53536a8ba38c
nnarct <nannapat1551@gmail.com>
2025-06-12 03:00:47 +0700

    feat: add queries refetch function

--- frontend/src/components/feature/Case/CaseFilesList/invalidateCaseQueries.ts
+++ frontend/src/components/feature/Case/CaseFilesList/invalidateCaseQueries.ts
@@ -6,3 +6,10 @@ export const invalidateCaseQueries = (caseId: string) => {
   queryClient.invalidateQueries({ queryKey: ['caseFilesByCaseId', caseId] });
   queryClient.invalidateQueries({ queryKey: ['caseModelByCaseId', caseId] });
 };
+
+export const refetchCaseQueries = async (caseId: string) => {
+  await queryClient.refetchQueries({ queryKey: ['case', caseId] });
+  await queryClient.refetchQueries({ queryKey: ['case-file-versions'] });
+  await queryClient.refetchQueries({ queryKey: ['caseFilesByCaseId', caseId] });
+  await queryClient.refetchQueries({ queryKey: ['caseModelByCaseId', caseId] });
+};
